f450cbe9609f4aff45ff3fed663f7206cb050c0a
FR <137614590+FilipRuman@users.noreply.github.com>
2025-11-28 11:47:41 +0100

    upd

--- corne/keymaps/default/keymap.c
+++ corne/keymaps/default/keymap.c
@@ -10,73 +10,72 @@ _SYMB,
 _MS
 };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE] = LAYOUT(
+        // left
+        LT(_SYMB, KC_SPC),  KC_Z,       KC_A,        KC_Q,
+        LCTL_T(KC_F2),      KC_X,       KC_S,        KC_W,
+        LGUI_T(KC_ENTER),   KC_C,       KC_D,        KC_E,
+                            KC_V,       KC_F,        KC_R,
+                            KC_B,       KC_G,        LT(_MS, KC_T),
 
-        [_BASE] = LAYOUT(
-                 //left
-                 LT(_SYMB, KC_SPC),   KC_Z,   KC_A, KC_Q,
-                 LCTL_T(KC_F2),            KC_X,   KC_S, KC_W,
-                 LGUI_T(KC_ENTER), KC_C,   KC_D, KC_E,
-                            KC_V,   KC_F, KC_R,
-                            KC_B,   KC_G, LT(_MS, KC_T),
-                 //right
-                 RALT_T(KC_ESC),              KC_N,       KC_H,         KC_Y,
-                 LT(_FUNC, KC_DEL),   KC_M,       KC_J,         KC_U,
-                 LSFT_T( KC_BSPC),              KC_COMMA,     KC_K,         KC_I,
-                                       KC_DOT,   KC_L, KC_O,
-                                       KC_SLASH,   KC_SEMICOLON,      KC_P
-        ),
-
-
-        [_FUNC] = LAYOUT(
-
-                //left
-                _______,            KC_MEDIA_NEXT_TRACK,    KC_AUDIO_MUTE,  _______,
-                _______,            KC_AUDIO_VOL_DOWN,      KC_AUDIO_VOL_UP,_______,
-                _______,            KC_TAB,             KC_MEDIA_PLAY_PAUSE,_______,
-                KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP,_______,
-                _______,                     _______,_______,
-                //right
-                _______,    _______,KC_LEFT,_______,
-                _______,    _______,KC_DOWN,_______,
-                _______,    _______,KC_UP,_______,
-                _______,    KC_RIGHT,_______,
-                _______,    _______,QK_BOOT
-        ),
+        // right
+        RALT_T(KC_ESC),     KC_N,       KC_H,        KC_Y,
+        LT(_FUNC, KC_DEL),  KC_M,       KC_J,        KC_U,
+        LSFT_T(KC_BSPC),    KC_COMMA,   KC_K,        KC_I,
+                            KC_DOT,     KC_L,        KC_O,
+                            KC_SLASH,   KC_SEMICOLON,KC_P
+    ),
 
 
+    [_FUNC] = LAYOUT(
+        // left
+        _______,            KC_MEDIA_NEXT_TRACK, KC_AUDIO_MUTE,      XXXXXXX,
+        _______,            KC_AUDIO_VOL_DOWN,   KC_AUDIO_VOL_UP,    XXXXXXX,
+        _______,            KC_BRIGHTNESS_DOWN,  KC_MEDIA_PLAY_PAUSE,XXXXXXX,
+                            KC_BRIGHTNESS_DOWN,  KC_TAB,             KC_UNDS,
+                            XXXXXXX,             XXXXXXX,            XXXXXXX,
 
-        [_SYMB] = LAYOUT(
-                //left
-                _______,        _______,        KC_9,   _______,
-                _______,        KC_NONUS_HASH,  KC_0,   _______,
-                _______,        KC_LEFT_BRACKET,KC_1,   KC_QUOT,
-                                KC_RIGHT_BRACKET,KC_2,   _______,
-                                _______,        KC_3,   _______,
-                //right
-                _______,    KC_MINUS,       KC_4,   KC_GRV,
-                _______,    KC_EQUAL,       KC_5,   KC_ASTR,
-                _______,    KC_BACKSLASH,   KC_6,   _______,
-                        _______,            KC_7,   _______,
-                        _______,            KC_8,   QK_BOOT
+        // right
+        _______,            XXXXXXX,             KC_LEFT,            XXXXXXX,
+        _______,            KC_NONUS_HASH,       KC_DOWN,            KC_MINUS,
+        _______,            XXXXXXX,             KC_UP,              KC_EQUAL,
+                            XXXXXXX,             KC_RIGHT,           XXXXXXX,
+                            XXXXXXX,             XXXXXXX,            QK_BOOT
+    ),
 
-        ),
 
-        [_MS] = LAYOUT(
-                //left
-                _______,        _______,        _______,   _______,
-                _______,        _______,  _______,   _______,
-                _______,        _______,_______,   _______,
-                                _______,_______,   _______,
-                                _______,        _______,_______,
-                //right
-                _______,MS_BTN1,MS_LEFT,   _______,
-                _______,MS_BTN2,MS_DOWN,  _______,
-                _______,_______,MS_UP,  _______,
-                        _______,MS_RGHT,   _______,
-                        _______,_______,_______
+    [_SYMB] = LAYOUT(
+        // left
+        _______,            XXXXXXX,             KC_9,               QK_BOOT,
+        _______,            XXXXXXX,             KC_0,               XXXXXXX,
+        _______,            KC_LEFT_BRACKET,     KC_1,               KC_QUOT,
+                            KC_RIGHT_BRACKET,    KC_2,               KC_GRV,
+                            XXXXXXX,             KC_3,               XXXXXXX,
 
-        )
+        // right
+        _______,            XXXXXXX,             KC_4,               XXXXXXX,
+        _______,            KC_HASH,             KC_5,               KC_ASTR,
+        _______,            KC_BACKSLASH,        KC_6,               KC_PIPE,
+                            XXXXXXX,             KC_7,               KC_AMPR,
+                            XXXXXXX,             KC_8,               XXXXXXX
+    ),
 
+
+    [_MS] = LAYOUT(
+        // left
+        _______,            XXXXXXX,             XXXXXXX,            XXXXXXX,
+        _______,            XXXXXXX,             XXXXXXX,            XXXXXXX,
+        _______,            XXXXXXX,             XXXXXXX,            XXXXXXX,
+                            XXXXXXX,             XXXXXXX,            XXXXXXX,
+                            XXXXXXX,             XXXXXXX,            XXXXXXX,
+
+        // right
+        _______,            MS_BTN1,             MS_LEFT,            XXXXXXX,
+        _______,            MS_BTN2,             MS_DOWN,            XXXXXXX,
+        _______,            XXXXXXX,             MS_UP,              XXXXXXX,
+                            XXXXXXX,             MS_RGHT,            XXXXXXX,
+                            XXXXXXX,             XXXXXXX,            XXXXXXX
+    )
 };
 void keyboard_post_init_user(void) {
   // Customise these values to desired behaviour
